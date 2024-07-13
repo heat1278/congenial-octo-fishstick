f950b9c53f0f3976069ef729c46d1a15a580ec92
heat1278 <52386814+heat1278@users.noreply.github.com>
2024-07-12 23:07:13 -0400

    Create hello_world.py
    First course as a proof of concept using print and input commands

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+#Ask user for their name.
+name = input("What is your name? \n")
+
+#Remove white space from name and make proper case
+name = name.title().strip()
+
+#say hello to the user.
+print (f"Hello, {name}")
+ 
